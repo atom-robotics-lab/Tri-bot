12021330d8585730306e4aea32280e1c4d87636e
vanshgupta4452 <150588000+vanshgupta4452@users.noreply.github.com>
2024-08-27 13:03:16 +0530

    SHOWCASE

--- motors/motors.ino
+++ motors/motors.ino
@@ -1,10 +1,10 @@
 //ch3 ch2 is forwardback, ch1 ch4 leftright
-int lowCh1 = 1180;
-int midCh1 = 1465;
-int highCh1 = 1975;
-int lowCh3 = 1019;
-int midCh3 = 1400;
-int highCh3 = 1800;
+int lowCh1 = 1087;
+int midCh1 = 1453;
+int highCh1 = 1826;
+int lowCh3 = 1020;
+int midCh3 = 1395;
+int highCh3 = 1798;
 int ping = 20;
 int ch1;//throttle
 int ch3;//L/R
@@ -85,14 +85,10 @@ void forwardbackward() {
       Serial.print("BACKWARD");
       Serial.print(val1);
       digitalWrite(INA1, HIGH);
-      digitalWrite(INB1, LOW); //Motor 1 forward 
-      // digitalWrite(INA2, HIGH);
-      // digitalWrite(INB2, LOW); //Motor 2 forward
-      // analogWrite(P1, val1);
-      // analogWrite(P2, val1);  
+      digitalWrite(INB1, LOW); 
       analogWrite(P1, 255);
-      analogWrite(RP, 0);
-      analogWrite(LP, 255);
+      analogWrite(RP, 255);
+      analogWrite(LP, 0);
        
   }
   //forward
@@ -109,8 +105,8 @@ void forwardbackward() {
     // analogWrite(P1, val2);
     // analogWrite(P2, val2);  
     analogWrite(P1, 255);
-    analogWrite(RP, 255);
-    analogWrite(LP, 0);  
+    analogWrite(RP, 0);
+    analogWrite(LP, 255);  
   } 
   Serial.println("");
 }
@@ -119,35 +115,38 @@ void forwardbackward() {
 void leftright() {
   // right
   if (ch1>=midCh1+ping && ch1<=highCh1) {
-      int val3 = map(ch1, midCh1+ping, highCh1, 0, 255);
-
-      Serial.print("RIGHT");
-      Serial.println(val3);
-      Serial.print(val3);
-      digitalWrite(INA1, HIGH);
-      digitalWrite(INB1, LOW); //Motor 1 forward 
-      // digitalWrite(INA2, LOW);
-      // digitalWrite(INB2, HIGH); //Motor 2 backward
-      // analogWrite(P1, val3);
-      // analogWrite(P2, val3);
-      analogWrite(P1, 255);
-      analogWrite(RP, 255);
-      analogWrite(LP, 0);
-  }
-    // left
-  if (ch1>=lowCh1 && ch1<=midCh1-ping) {
-    int val4 = map(ch1, midCh1-ping, lowCh1, 0, 255);
-    Serial.print("LEFT");
-    Serial.println(val4);
-      digitalWrite(INA1, LOW); // motor bec
-      digitalWrite(INB1, HIGH); 
+      int val4 = map(ch1, midCh1-ping, lowCh1, 0, 255);
+      Serial.print("LEFT");
+      Serial.println(val4);
+      digitalWrite(INA1, HIGH); // motor bec
+      digitalWrite(INB1, LOW); 
       // digitalWrite(INA2, HIGH); //motor for
       // digitalWrite(INB2, LOW); 
       // analogWrite(P1, val4);
       // analogWrite(P2, val4);  
       analogWrite(P1, 255);
       analogWrite(RP, 0);
-      analogWrite(LP, 255); 
+      analogWrite(LP, 255);
+      
+      
+      
+  }
+    // left
+  if (ch1>=lowCh1 && ch1<=midCh1-ping) {
+  int val3 = map(ch1, midCh1+ping, highCh1, 0, 255);
+      
+      Serial.print("RIGHT");
+      Serial.println(val3);
+      Serial.print(val3);
+      digitalWrite(INA1, LOW);
+      digitalWrite(INB1, HIGH); //Motor 1 forward 
+      // digitalWrite(INA2, LOW);
+      // digitalWrite(INB2, HIGH); //Motor 2 backward
+      // analogWrite(P1, val3);
+      // analogWrite(P2, val3);
+      analogWrite(P1, 255);
+      analogWrite(RP, 255);
+      analogWrite(LP, 0); 
 
   }
   Serial.println("");
